281043ca5e66b3b211eda9fbf64898f52c59e95f
Andrew Hsiao <hsiao@thumbtack.com>
2017-09-02 15:08:51 -0700

    Update and format loader.js

--- loader.js
+++ loader.js
@@ -1,147 +1,131 @@
 var model = {
-	title: 'LAW',
-	featuredContent:                		{
-			image: '/images/spin.jpg',
-			title: 'After Hours',
-			id: '158818287',
-			type: 'vimeo'
-		},
-	contentPages: [{
-		title: 'COMMERCIAL',
-		content: [
-	        {
-		        image: '/images/bombaySQ.jpg',
-                        altImage: '/images/bombayBW.jpg',
-		        title: 'Bombay Sapphire - Artists',
-		        id: 'enJGxn25Qoc',
-		        type: 'youtube',
-	        },
-		{
-			image: '/images/FLtweet.jpg',
-			altImage: '/images/FLtweetBW.jpg',
-			title: 'Foot Locker - Harden',
-			id: 'pznnxBkyhdc',
-			type: 'youtube'			
-		},
-		{
-			image: '/images/GEChinaSQ.jpg',
-			altImage: '/images/GEChinaBW.jpg',
-			title: 'GE China - First Flight',
-			id: '134998540',
-			type: 'vimeo',
-			youtube: 'http://www.youtube.com/watch?v=fW6Wa6ccXq8',
-                        zh: {
-			          title: 'GE China - First Flight',
-			          id: 'XMTI5ODM2NTU0MA',
-			          type: 'youku'
-		        }	
-		},
-		{
-			image: '/images/LifeAfterSQ.jpg',
-			altImage: '/images/LifeAfterBW.jpg',
-			title: 'GE - LifeAfter',
-			id: '192968803',
-			type: 'vimeo',
-		},
-		{
-			image: '/images/HardenSQ.jpg',
-			altImage: '/images/HardenBW.jpg',
-			title: 'Foot Locker - The Beards',
-			id: '93106718',
-			type: 'vimeo',
-			youtube: 'http://www.youtube.com/watch?v=UmU2WSEM-iw',
-                        zh: {
-			          title: 'Foot Locker - The Beards',
-			          id: 'XMTI5ODM3NTA0OA',
-			          type: 'youku'
-		        }
-		},
-                {
-			image: '/images/CarverSQ.jpg',
-			altImage: '/images/CarverBW.jpg',
-			title: 'Snickers - Carver',
-			id: '156516678',
-			type: 'vimeo'
-		}]
-	},
-	{
-		title: 'NARRATIVE',
-		content: [
-		{
-			image: '/images/DeimosSQ.jpg',
-			altImage: '/images/DeimosBW.jpg',
-			title: 'WE HUMANS',
-			id: '173789253',
-			type: 'vimeo'
-		},
-                {
-			image: '/images/spinSQ.jpg',
-			altImage: '/images/spinBW.jpg',
-			title: 'spin',
-			id: '158348485',
-			type: 'vimeo'
-		},
-                {
-			image: '/images/IDMTRSQ.jpg',
-			altImage: '/images/IDMTRBW.jpg',
-			title: 'I Dont Make The Rules',
-			id: '216448302',
-			type: 'vimeo'
-		},]
-
-	},
+    title: 'LAW',
+    featuredContent: {
+        image: '/images/spin.jpg',
+        title: 'After Hours',
+        id: '158818287',
+        type: 'vimeo',
+    },
+    contentPages: [
         {
-		title: 'MUSIC VIDEO',
-		content: [{
-		        image: '/images/ScaredSQ.jpg',
-                        altImage: '/images/ScaredBW.jpg',
-		        title: 'Delta Rae - Scared',
-		        id: '119471879',
-		        type: 'vimeo',
-                        youtube: 'http://www.youtube.com/watch?v=dykkZteE2Hw',
-                        zh: {
-			          title: 'Delta Rae - Scared',
-			          id: 'XMTI5ODM4NzYwOA',
-			          type: 'youku'
-		        }
-	        },
+            title: 'COMMERCIAL',
+            content: [
                 {
-		        image: '/images/PosnerSQ.jpg',
-                        altImage: '/images/PosnerBW.jpg',
-		        title: 'Mike Posner - My Light',
-		        id: '135018169',
-		        type: 'vimeo',
-                        youtube: 'http://www.youtube.com/watch?v=PY424feYbas',
-                        zh: {
-			          title: 'Mike Posner - My Light',
-			          id: 'XMTI5ODM4ODg2NA',
-			          type: 'youku'
-		        }
-	        },
-		{
-			image: '/images/DeltaSQ.jpg',
-			altImage: '/images/DeltaBW.jpg',
-			title: 'DR - Bottom of the River',
-			id: '73015975',
-			type: 'vimeo',
-			youtube: 'http://www.youtube.com/watch?v=bimam2j2gEg',
-                        zh: {
-			          title: 'DR - Bottom of the River',
-			          id: 'XNjA0NTEyNTcy',
-			          type: 'youku'
-		        }
-		}]
-	 }],
-	infoPages: [{
-		title: 'Award',
-		html: 'Test.'
-	},
-	{
-		title: 'About',
-		html: 'Lawrence Chen is an award winning filmmaker from Beijing, China and New York City. He is currently the lead commercial director at BBDO NY and also the 8th most awarded director at the 2015 Cannes Lions. He has garnered multiple awards at the Cannes Lions, Clios, One Show, New Director Showcase, and mentions at the Cannes, Rome, London, Tribeca, New York and Sundance film festivals. Lawrence also directs music videos, short films, and documentaries, has done projects around the world and has traveled to over 50 countries and counting. He has recently developed an online tool to help filmmakers find the best digital cinema camera to shoot with: <a href="http://cinesurf.co">CINESURF</a>.'
-	},
-	{
-		title: 'Contact',
-		html: 'boss@lawrencechenfilms.com'
-	}]
+                    image: '/images/GEChinaSQ.jpg',
+                    altImage: '/images/GEChinaBW.jpg',
+                    title: 'GE China - First Flight',
+                    id: '134998540',
+                    type: 'vimeo',
+                    youtube: 'http://www.youtube.com/watch?v=fW6Wa6ccXq8',
+                    zh: {
+                        title: 'GE China - First Flight',
+                        id: 'XMTI5ODM2NTU0MA',
+                        type: 'youku',
+                    },
+                },
+                {
+                    image: '/images/LifeAfterSQ.jpg',
+                    altImage: '/images/LifeAfterBW.jpg',
+                    title: 'GE - LifeAfter',
+                    id: '192968803',
+                    type: 'vimeo',
+                },
+                {
+                    image: '/images/SteveSQ.jpg',
+                    altImage: '/images/SteveBW.jpg',
+                    title: 'AT&T - Steve',
+                    id: '205138997',
+                    type: 'vimeo',
+                },
+                {
+                    image: '/images/NBCSQ.jpg',
+                    altImage: '/images/NBCBW.jpg',
+                    title: 'The NBC - Rehearsal',
+                    id: '225877245',
+                    type: 'vimeo',
+                },
+            ],
+        },
+        {
+            title: 'NARRATIVE',
+            content: [
+                {
+                    image: '/images/IDMTRSQ.jpg',
+                    altImage: '/images/IDMTRBW.jpg',
+                    title: 'I Dont Make The Rules',
+                    id: '216448302',
+                    type: 'vimeo',
+                },
+                {
+                    image: '/images/DeimosSQ.jpg',
+                    altImage: '/images/DeimosBW.jpg',
+                    title: 'WE HUMANS',
+                    id: '151486398',
+                    type: 'vimeo',
+                },
+                {
+                    image: '/images/spinSQ.jpg',
+                    altImage: '/images/spinBW.jpg',
+                    title: 'spin',
+                    id: '158348485',
+                    type: 'vimeo',
+                },
+            ],
+        },
+        {
+            title: 'MUSIC VIDEO',
+            content: [
+                {
+                    image: '/images/ScaredSQ.jpg',
+                    altImage: '/images/ScaredBW.jpg',
+                    title: 'Delta Rae - Scared',
+                    id: '119471879',
+                    type: 'vimeo',
+                    youtube: 'http://www.youtube.com/watch?v=dykkZteE2Hw',
+                    zh: {
+                        title: 'Delta Rae - Scared',
+                        id: 'XMTI5ODM4NzYwOA',
+                        type: 'youku',
+                    },
+                },
+                {
+                    image: '/images/PosnerSQ.jpg',
+                    altImage: '/images/PosnerBW.jpg',
+                    title: 'Mike Posner - My Light',
+                    id: '135018169',
+                    type: 'vimeo',
+                    youtube: 'http://www.youtube.com/watch?v=PY424feYbas',
+                    zh: {
+                        title: 'Mike Posner - My Light',
+                        id: 'XMTI5ODM4ODg2NA',
+                        type: 'youku',
+                    },
+                },
+                {
+                    image: '/images/DeltaSQ.jpg',
+                    altImage: '/images/DeltaBW.jpg',
+                    title: 'DR - Bottom of the River',
+                    id: '73015975',
+                    type: 'vimeo',
+                    youtube: 'http://www.youtube.com/watch?v=bimam2j2gEg',
+                    zh: {
+                        title: 'DR - Bottom of the River',
+                        id: 'XNjA0NTEyNTcy',
+                        type: 'youku',
+                    },
+                },
+            ],
+        },
+    ],
+    infoPages: [
+        {
+            title: 'About',
+            html:
+                'Lawrence Chen is an award winning filmmaker from Beijing, China and New York City. He is currently the lead commercial director at BBDO NY and also the 8th most awarded director at the 2015 Cannes Lions. He has garnered multiple awards at the Cannes Lions, Clios, One Show, New Director Showcase, and mentions at the Cannes, Rome, London, Tribeca, New York and Sundance film festivals. Lawrence also directs music videos, short films, and documentaries, has done projects around the world and has traveled to over 50 countries and counting. He has recently developed an online tool to help filmmakers find the best digital cinema camera to shoot with: <a href="http://cinesurf.co">CINESURF</a>.',
+        },
+        {
+            title: 'Contact',
+            html: 'thelawchen@gmail.com',
+        },
+    ],
 };
