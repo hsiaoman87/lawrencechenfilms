b785e996fa9028733899508709384411e21623da
thelawchen <thelawchen@gmail.com>
2022-04-18 20:55:43 -0400

    Update loader.js
    Remove UTA

--- loader.js
+++ loader.js
@@ -97,7 +97,7 @@ var model = {
         },
         {
             title: 'Contact',
-            html: 'Commercial: Stink Films (desmond@stink.asia) <br> Film & TV: UTA (MichaelM@unitedtalent.com) <br> Personal: Me (thelawchen@gmail.com)',
+            html: 'Commercial: Stink Films (desmond@stink.asia) <br> Personal: Me (thelawchen@gmail.com)',
         },
     ],
 };
